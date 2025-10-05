4ca813842b6ae077665567af7399ac64c9d88f0b
Ninomae <55.swoosh_pupal@icloud.com>
2025-10-05 22:08:01 +0800

    Update Muse.cpp
    revise syntax, add VAR, change sound generator, etc.

--- Muse.cpp
+++ Muse.cpp
@@ -1,57 +1,252 @@
-#include<bits/stdc++.h>
-#include<Windows.h>
+/* COMPILE BY DOING THIS IF USING G++:
+   g++ Muse.cpp -ldsound -ldxguid -lwinmm -lole32 -luuid -static -o playtone.exe
+*/
+#include <bits/stdc++.h>
+#include <Windows.h>
+#include <dsound.h>
+#include <thread>
 using namespace std;
 
-//Initial Status
-int octave=4;
-int transpose=0;
-int bpm=120;
-int note=4;
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
 
-set<char> keys={'c','d','e','f','g','a','b'};
+#pragma comment(lib, "dsound.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "winmm.lib")
 
-map<char,double> oct4={
-    {'c',261.63},
-    {'d',293.66},
-    {'e',329.63},
-    {'f',349.23},
-    {'g',392.00},
-    {'a',440.00},
-    {'b',493.88},
+// ---------------------------------------------
+// DirectSound tone generator
+// ---------------------------------------------
+void playTone(double frequency, double durationMs) {
+    double durationSeconds = durationMs / 1000.0;
+
+    LPDIRECTSOUND8 directSound;
+    if (FAILED(DirectSoundCreate8(NULL, &directSound, NULL))) return;
+    directSound->SetCooperativeLevel(GetDesktopWindow(), DSSCL_PRIORITY);
+
+    DSBUFFERDESC primaryDesc = {};
+    primaryDesc.dwSize = sizeof(DSBUFFERDESC);
+    primaryDesc.dwFlags = DSBCAPS_PRIMARYBUFFER;
+
+    LPDIRECTSOUNDBUFFER primaryBuffer;
+    if (FAILED(directSound->CreateSoundBuffer(&primaryDesc, &primaryBuffer, NULL))) {
+        directSound->Release();
+        return;
+    }
+
+    WAVEFORMATEX waveFormat = {};
+    waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+    waveFormat.nChannels = 1;
+    waveFormat.nSamplesPerSec = 44100;
+    waveFormat.wBitsPerSample = 16;
+    waveFormat.nBlockAlign = waveFormat.nChannels * waveFormat.wBitsPerSample / 8;
+    waveFormat.nAvgBytesPerSec = waveFormat.nSamplesPerSec * waveFormat.nBlockAlign;
+    primaryBuffer->SetFormat(&waveFormat);
+
+    int sampleRate = waveFormat.nSamplesPerSec;
+    int numSamples = static_cast<int>(sampleRate * durationSeconds);
+    int bufferSize = numSamples * waveFormat.nBlockAlign;
+
+    DSBUFFERDESC secondaryDesc = {};
+    secondaryDesc.dwSize = sizeof(DSBUFFERDESC);
+    secondaryDesc.dwFlags = DSBCAPS_GLOBALFOCUS;
+    secondaryDesc.dwBufferBytes = bufferSize;
+    secondaryDesc.lpwfxFormat = &waveFormat;
+
+    LPDIRECTSOUNDBUFFER secondaryBuffer;
+    if (FAILED(directSound->CreateSoundBuffer(&secondaryDesc, &secondaryBuffer, NULL))) {
+        primaryBuffer->Release();
+        directSound->Release();
+        return;
+    }
+
+    vector<short> samples(numSamples);
+    const double amplitude = 30000.0;
+    int fadeSamples = sampleRate * 0.01;
+
+    for (int i = 0; i < numSamples; ++i) {
+        double t = (double)i / sampleRate;
+        double value = sin(2.0 * M_PI * frequency * t);
+        double gain = 1.0;
+        if (i < fadeSamples) gain = (double)i / fadeSamples;
+        else if (i > numSamples - fadeSamples) gain = (double)(numSamples - i) / fadeSamples;
+        samples[i] = (short)(value * amplitude * gain);
+    }
+
+    void* ptr1; DWORD bytes1; void* ptr2; DWORD bytes2;
+    if (SUCCEEDED(secondaryBuffer->Lock(0, bufferSize, &ptr1, &bytes1, &ptr2, &bytes2, 0))) {
+        memcpy(ptr1, samples.data(), bytes1);
+        if (ptr2 && bytes2 > 0)
+            memcpy(ptr2, (char*)samples.data() + bytes1, bytes2);
+        secondaryBuffer->Unlock(ptr1, bytes1, ptr2, bytes2);
+    }
+
+    secondaryBuffer->SetCurrentPosition(0);
+    secondaryBuffer->Play(0, 0, 0);
+    Sleep((DWORD)(durationSeconds * 1000));
+
+    secondaryBuffer->Release();
+    primaryBuffer->Release();
+    directSound->Release();
+}
+
+// ---------------------------------------------
+// Global state
+// ---------------------------------------------
+ifstream fin;
+ofstream fout;
+
+int octave = 4;
+int transpose = 0;
+int bpm = 100;
+int note = 4;
+
+set<char> keys = {'c','d','e','f','g','a','b'};
+
+map<char,double> oct4 = {
+    {'c',261.63}, {'d',293.66}, {'e',329.63},
+    {'f',349.23}, {'g',392.00}, {'a',440.00}, {'b',493.88}
 };
 
+map<string,string> var_mapping;
+
+// ---------------------------------------------
+// Helpers
+// ---------------------------------------------
 void ofreq(const char &m){
-    double note_value=60000.0/bpm*4.0/note;
-    double freq=oct4[m]*pow(2,octave-4);
-
-    Beep(freq,note_value);
+    double note_value = 60000.0 / bpm * 4.0 / note;
+    double freq = oct4[m] * pow(2, octave - 4) * pow(2, transpose / 12.0);
+    //cout << "Playing " << m << octave << " (" << freq << " Hz)\n";
+    playTone(freq, note_value);
 }
 
-void check(const string &opr){
-    int oprlen=opr.length();
-    if(keys.count(opr[0])==1){ofreq(opr[0]);}
-    else if(opr[0]=='O'){octave = int(opr[1]-'0');}
-    else if(opr[0]=='N'){ //setting note value 1,2,4,8,16 etc;
-        int note_temp=0;
-        for(int i=1;i<=oprlen-1;i++){
-            note_temp+=int(opr[i]-'0')*int(pow(10,oprlen-i-1));
+// ---------------------------------------------
+// Process a command line
+// ---------------------------------------------
+void process_line(const string &line) {
+    istringstream iss(line);
+    string opr;
+
+    while (iss >> opr) {
+        if (keys.count(opr[0])) {
+            ofreq(opr[0]);
         }
-        note=note_temp;
-    }
-    else if(opr[0]=='>'){octave++;}
-    else if(opr[0]=='<'){octave--;}
+        else if (opr == "VAR") {
+            string var_name;
+            string var_content;
+            iss >> var_name;
+            char c;
+            iss >> c; // skip '('
+            while (iss >> noskipws >> c && c != ')') {
+                var_content += c;
+            }
+            var_mapping[var_name] = var_content;
+            cout << "Defined VAR " << var_name << " = " << var_content << endl;
+        }
+        else if (opr[0] == '/') {
+            string var_to_exec = opr.substr(1); // fixed: take name after '/'
+            auto it = var_mapping.find(var_to_exec);
+            if (it == var_mapping.end()) {
+                cerr << "Variable " << var_to_exec << " not found.\n";
+            } else {
+                // recursively execute the content
+                process_line(it->second);
+            }
+        }
+        else if (opr[0] == '>') octave++;
+        else if (opr[0] == '<') octave--;
+        else if (opr[0] == 'T') {
+            string trans_temp;
+            if (!(iss >> trans_temp)) {
+                cerr << "Error: transpose value missing\n";
+                continue;
+            }
 
+            if (trans_temp == "++") {
+                transpose++;
+            } else if (trans_temp == "--") {
+                transpose--;
+            } else {
+                try {
+                    transpose = stoi(trans_temp);
+                } catch (...) {
+                    cerr << "Invalid transpose value: " << trans_temp << endl;
+                    // keep previous transpose
+                }
+        }
+
+        cout << "Transpose set to " << transpose << endl;
+    }
+        else if (opr[0] == 'N') iss >> note;
+        else if (opr[0] == 'O') {
+            char c; iss >> c;
+            if (isdigit(c)) octave = c - '0';
+        }
+        else if (opr == "quit") {
+            cout << "Exiting program..." << endl;
+            exit(0);
+        }
+    }
 }
 
-int main(){
-    while(1){
-        string opr;
-        cin>>opr;
-        if(opr=="quit"){return 0;}
-        check(opr);
+// ---------------------------------------------
+// Thread 1: watch data.txt
+// ---------------------------------------------
+void file_watcher() {
+    streampos lastPos = 0;
+    while (true) {
+        fin.clear();
+        fin.seekg(lastPos);
+
+        string line;
+        while (getline(fin, line)) {
+            if (!line.empty()) process_line(line);
+        }
+
+        lastPos = fin.tellg();
+        fin.clear();
+        this_thread::sleep_for(chrono::milliseconds(200));
+    }
+}
+
+// ---------------------------------------------
+// Thread 2: interactive whole-line input
+// ---------------------------------------------
+void interactive_input() {
+    cout << "Interactive mode: type commands or notes.\n"
+         << "Examples:\n"
+         << "   c d e f g\n"
+         << "   VAR tune (c d e)\n"
+         << "   /tune\n"
+         << "   quit\n"
+         << "-------------------------------------------\n";
+
+    string line;
+    while (true) {
+        cout << "> ";
+        if (!getline(cin, line)) break;
+        if (!line.empty()) process_line(line);
+    }
+}
+
+// ---------------------------------------------
+// Main
+// ---------------------------------------------
+int main() {
+    fin.open("data.txt");
+    fout.open("data.txt", ios::app);
+    if (!fin.is_open() || !fout.is_open()) {
+        cerr << "Error opening data.txt\n";
+        return 1;
     }
 
+    cout << "Muse is running. Edit data.txt or type directly below.\n";
 
+    thread t1(file_watcher);
+    thread t2(interactive_input);
 
+    t1.join();
+    t2.join();
     return 0;
 }
