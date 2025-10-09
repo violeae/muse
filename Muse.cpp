295a8d84c63b993be21636e1f821c27786b176c9
Ninomae <55.swoosh_pupal@icloud.com>
2025-10-09 22:29:28 +0800

    Update Muse.cpp
    support instruments
    allow channels to support simultaneous playback,
    use threads now,
    update {} and () usage,
    support for scopes on NUMs and VARs

--- Muse.cpp
+++ Muse.cpp
@@ -3,19 +3,92 @@
 #include <chrono>
 #include <Windows.h>
 #include <thread>
+#include<atomic>
 using namespace std;
 
-
-// --- Definition of constants
 #ifndef M_PI
 #define M_PI 3.14159265358979323846
 #endif
+
+// -- Thread-local channel allocation
+atomic<int> nextChannel{0};
+int getThreadChannel(){
+    int ch = nextChannel++;
+    ch%=16;
+    if(ch==9){ ch = nextChannel++ % 16;}
+    return ch;
+}
+
+// ---------------------------------------------
+// Scope Structure
+// ---------------------------------------------
+
+struct ScopeNode{
+    map<string, int> numVars;
+    map<string, string> varVars;
+    int channel =-1;
+    ScopeNode* parent = nullptr;
+    vector<ScopeNode*> children;
+};
+
+
+
+set<char> keys = {'c','d','e','f','g','a','b'};
+
+map<char,double> oct4 = {
+    {'c',261.63}, {'d',293.66}, {'e',329.63},
+    {'f',349.23}, {'g',392.00}, {'a',440.00}, {'b',493.88},
+};
+
+
+
+
+int getNum(ScopeNode* scope, const string& name){
+    for(auto* cur = scope; cur; cur = cur->parent){
+        auto it = cur->numVars.find(name);
+        if (it != cur->numVars.end()){
+            int value = it->second;
+            if(scope!=cur){
+                scope->numVars[name] = value;
+            }
+            return value;
+        }
+    }
+    throw runtime_error("NUM: "+name+" not found");
+}
+
+string getVar(ScopeNode* scope, const string& name){
+    for(auto* cur =scope; cur; cur = cur->parent){
+        if(cur->varVars.count(name)){
+            return cur->varVars[name];
+        }
+    }
+    throw runtime_error("VAR: "+name+" not found");
+}
+
+bool hasNum(ScopeNode* scope, const std::string& name) {
+    for (auto* cur = scope; cur; cur = cur->parent) {
+        if (cur->numVars.find(name) != cur->numVars.end())
+            return true;
+    }
+    return false;
+}
+
+bool hasVar(ScopeNode* scope, const std::string& name) {
+    for (auto* cur = scope; cur; cur = cur->parent) {
+        if (cur->varVars.find(name) != cur->varVars.end())
+            return true;
+    }
+    return false;
+}
 
 
 
 // --- Initialize FluidSynth globally (once)
 fluid_settings_t* g_settings = nullptr;
 fluid_synth_t* g_synth = nullptr;
+
+
 
 // Call this once when program starts
 void initFluidSynth(const char* soundfontPath) {
@@ -35,19 +108,18 @@ int freqToMidi(double freq) {
 }
 
 // --- Play a note by frequency (ms) with instrument timbre
-void playTone(double freq, int durationMs, int velocity = 100) {
-    if (!g_synth) return;
-
+void playTone(double freq, int durationMs, ScopeNode* scope) {
     int midiNote = freqToMidi(freq);
-
+    int channel = scope->channel;
+    int velocity = getNum(scope,"V");
+    int instrument = getNum(scope,"I");
+    fluid_synth_program_change(g_synth, channel, instrument);
     // Start note
-    fluid_synth_noteon(g_synth, 0, midiNote, velocity);
-
+    fluid_synth_noteon(g_synth, channel, midiNote, velocity);
     // Wait for specified duration
-    Sleep(durationMs);
-
+    this_thread::sleep_for(chrono::milliseconds(durationMs));
     // Stop note
-    fluid_synth_noteoff(g_synth, 0, midiNote);
+    fluid_synth_noteoff(g_synth, channel, midiNote);
 }
 
 // --- Cleanup FluidSynth
@@ -63,44 +135,36 @@ void cleanupFluidSynth() {
 ifstream fin;
 ofstream fout;
 
-map<string,int> nums={
-    {"O", 4}, {"T", 0}, {"B", 140},
-    {"N", 4}
-};
+ScopeNode* root = new ScopeNode();
 
-set<char> keys = {'c','d','e','f','g','a','b'};
 
-map<char,double> oct4 = {
-    {'c',261.63}, {'d',293.66}, {'e',329.63},
-    {'f',349.23}, {'g',392.00}, {'a',440.00}, {'b',493.88},
-};
 
-map<string,string> var_mapping;
+
 
 // ---------------------------------------------
 // Helpers
 // ---------------------------------------------
-void ofreq(const char &m){
-    double note_value = 60000.0 / nums["B"] * 4.0 / nums["N"];
-    double freq = oct4[m] * pow(2, nums["O"] - 4) * pow(2, nums["T"] / 12.0);
+void ofreq(const char &m, ScopeNode* scope){
+    double note_value = 60000.0 / getNum(scope,"B") * 4.0 / (getNum(scope,"N"));
+    double freq = oct4[m] * pow(2, getNum(scope,"O") - 4) * pow(2, getNum(scope,"T") / 12.0);
     //cout << "Playing " << m << nums["O"] << " (" << freq << " Hz)\n";
-    playTone(freq, note_value);
+    playTone(freq, note_value, scope);
 }
 
 // ---------------------------------------------
 // Process a command line
 // ---------------------------------------------
-void process_line(const string &line) {
+void process_line(const string &line, ScopeNode* scope) {
     istringstream iss(line);
     string opr;
 
     while (iss >> opr) {
         // Keys
         if (keys.count(opr[0])) {
-            ofreq(opr[0]);
+            ofreq(opr[0], scope);
         }
         else if (opr == "r"){
-            Sleep(static_cast<DWORD>(60000.0 / nums["B"] * 4.0 / nums["N"]));
+            Sleep(static_cast<DWORD>(60000.0 / (getNum(scope,"B")) * 4.0 / (getNum(scope,"N")) ));
         }
         // Declaration
         else if (opr == "VAR") {
@@ -117,34 +181,50 @@ void process_line(const string &line) {
                 var_content += c;
             }
             iss >> std::skipws;
-            var_mapping[var_name] = var_content;
+            scope->varVars[var_name] = var_content;
             cout << "Defined VAR " << var_name << " = " << var_content << endl;
         }
         else if (opr == "NUM"){
             string num_name;
             int num_content;
             iss>>num_name>>num_content;
-            nums[num_name]=num_content;
+            scope->numVars[num_name]=num_content;
             cout<< "Define NUM "<<num_name<<" = "<< num_content<<endl;
         }
         else if (opr[0] == '/') {
             string var_to_exec = opr.substr(1); // fixed: take name after '/'
-            auto it = var_mapping.find(var_to_exec);
-            if (it == var_mapping.end()) {
-                cerr << "Variable " << var_to_exec << " not found.\n";
+            
+            if (hasVar==0) {
+                cerr << "VAR" << var_to_exec << " not found.\n";
             } else {
+                auto it = getVar(scope,var_to_exec);
                 // recursively execute the content
-                process_line(it->second);
+                process_line(it,scope);
             }
         }
 
         
-        else if (opr == ">") nums["O"]++;
-        else if (opr == "<") nums["O"]--;
+        else if (opr == ">") {
+            getNum(scope,"O");
+            scope->numVars["O"]++;
+        }
+        else if (opr == "<") {
+            getNum(scope,"O");
+            scope->numVars["O"]--;
+        }
 
 
         // Setting nums
-        else if (nums.find(opr)!=nums.end()){
+        else if (
+            (scope->numVars.find(opr)!=scope->numVars.end())
+            ||
+            (
+                scope->numVars.find(opr)==scope->numVars.end()
+                &&
+                hasNum(scope,opr)
+            )
+        ){
+            getNum(scope,opr);
             string num_temp;
             if(!(iss>>num_temp)){
                 cerr<<"Error: "<<opr<<" value missing\n";
@@ -152,27 +232,26 @@ void process_line(const string &line) {
             }
 
             if(num_temp== "++"){
-                nums[opr]++;
+                scope->numVars[opr]++;
             }
             else if (num_temp == "--"){
-                nums[opr]--;
+                scope->numVars[opr]--;
             }
             else if (num_temp == ">>"){
-                nums[opr]/=2;
+                scope->numVars[opr]/=2;
             }
             else if (num_temp == "<<"){
-                nums[opr]*=2;
+                scope->numVars[opr]*=2;
             }
             else {
                 try {
-                    nums[opr] = stoi(num_temp);
+                    scope->numVars[opr] = stoi(num_temp);
                 } catch(...) {
                     cerr << "Invalid "<<opr<<" value: "<<num_temp<<endl;
                 }
             }
-            cout<<"Num "<<opr<<" set to "<<nums[opr]<<endl;
+            cout<<"Num "<<opr<<" set to "<<scope->numVars[opr]<<endl;
         }
-
 
 
         else if (opr=="if"){
@@ -182,7 +261,7 @@ void process_line(const string &line) {
             char c;
             iss>>c; //skip '('
             int brc=1;
-            while (iss>>noskipws>>c){
+            while (iss>>noskipws>>c){ //checking depths
                 if(c=='('){brc++;}
                 else if(c==')'){brc--;}
                 if(brc==0){break;}
@@ -190,22 +269,94 @@ void process_line(const string &line) {
             }
             iss >> std::skipws;
             //Checking if conditions
-            auto it = nums.find(if_cond);
-            if(it == nums.end()){
+            auto it = scope->numVars.find(if_cond);
+            if(hasNum(scope,if_cond)==0){
                 cerr<<"Num "<<if_cond<<" not found.\n";
             }
             else{
-                if(nums[if_cond]!=0){
-                    process_line(if_sentence);
+                if(getNum(scope,if_cond)!=0){
+                    process_line(if_sentence,scope);
                 }
             }
         }
+        //Simultaneous Sounds
+        else if (opr == "{") {
+            vector<string> thread_contents;
+            string content;
+            char c;
+            int brace_depth=1;
+            while (iss >> noskipws >> c) {
+                if(c =='{') {
+                    content+=c;
+                    brace_depth++;
+                }
+                else if (c == '}') {
+                    brace_depth--;
+                    if(brace_depth==0){break;}
+                    else{content+=c;}
+                }
+                
+                else if (c == '('&&brace_depth==1) {
+                    // Only deal with top-level paren 
+                    if(!content.empty()){
+                        thread_contents.push_back(content);
+                        content.clear();
+                    }
+                    string par_content;
+                    int paren_depth = 1;
+                    while (iss >> c) {
+                        par_content += c;
+                        if (c == '(') paren_depth++;
+                        else if (c == ')') paren_depth--;
+                        if (paren_depth == 0) break;
+                        
+                    }
+                    
+                    thread_contents.push_back(par_content);
+                } 
+                else if (isspace(c)&&brace_depth==1){
+                    if(!content.empty()){
+                        thread_contents.push_back(content);
+                        content.clear();
+                    }
+                }
+                
+                else {
+                    content+=c;
+                }
+                
+            }
+            if(!content.empty()){thread_contents.push_back(content);}
+
+
+            // Launch subscopes
+            vector<thread> ths;
+            for(const auto& component: thread_contents){
+                ths.emplace_back([component,scope](){
+                    ScopeNode* node =new ScopeNode();
+                    node->parent = scope;
+                    node->channel = getThreadChannel();
+                    scope->children.push_back(node);
+                    process_line(component,node);
+                });
+            }
+            for (auto& th : ths) th.join();  // wait for all to finish
+        }
+
+
+
         // Printing and functions
         else if (opr == "info"){
-            cout<<"Current Settings: "<<"BPM: "<<nums["B"]<<" Octave: "<<nums["O"]<<endl
-                <<"Note Value: "<<nums["N"]<<" Transpose: "<<nums["T"]<<endl;
-            for(auto vm_pair: var_mapping){
-                cout<<"VAR "<<vm_pair.first<<" = "<<vm_pair.second<<endl;
+            cout<<"Current Settings: "<<"BPM: "<<getNum(scope,"B")<<" Octave: "<<getNum(scope,"O")<<endl
+                <<"Note Value: "<<getNum(scope,"N")<<" Transpose: "<<getNum(scope,"T")<<endl
+                <<"Instrument: "<<getNum(scope, "I")<<"Volume: "<<getNum(scope,"V")<<endl;
+            //Print All VARs
+            auto cur = scope;
+            while(cur!=nullptr){
+                for (const auto& [name, value]: cur->varVars){
+                    cout<<"VAR "<<name<<" = "<<value<<endl;
+                }
+                cur = cur->parent;
             }
         }
         else if (opr == "quit") {
@@ -226,7 +377,7 @@ void file_watcher() {
 
         string line;
         while (getline(fin, line)) {
-            if (!line.empty()) process_line(line);
+            if (!line.empty()) process_line(line,root);
         }
 
         lastPos = fin.tellg();
@@ -251,7 +402,7 @@ void interactive_input() {
     while (true) {
         cout << "> ";
         if (!getline(cin, line)) break;
-        if (!line.empty()) process_line(line);
+        if (!line.empty()) process_line(line,root);
     }
 }
 
@@ -259,8 +410,19 @@ void interactive_input() {
 // Main
 // ---------------------------------------------
 int main() {
+    
+    root->channel = getThreadChannel();
+    root->numVars = {
+        {"O", 4}, //Octave
+        {"T", 0}, //Transpose
+        {"B", 140}, //BPM
+        {"N", 4}, //Note Value
+        {"V", 100}, // Velocity
+        {"I", 0} //Instrument (Default: Piano)
+    };
+
     initFluidSynth("FluidR3_GM.sf2");
-    fluid_synth_program_change(g_synth, 0, 40);
+    fluid_synth_program_change(g_synth, 0,25);
     fin.open("data.txt");
     fout.open("data.txt", ios::app);
     if (!fin.is_open() || !fout.is_open()) {
